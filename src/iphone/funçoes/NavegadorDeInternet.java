8c4792e1bbfab62ea0c911f01d9bed70cab375cd
kaique <alglassvidracaria123@gmail.com>
2024-09-29 21:07:20 -0300

    aprendendo POO

---
+++ src/iphone/funçoes/NavegadorDeInternet.java
@@ -0,0 +1,16 @@
+package iphone.funçoes;
+
+public class NavegadorDeInternet {
+	public void exibirPagina(){
+		System.out.println("Exibindo a pagina");
+	}
+	
+	public void adicionarNovaPagina(){
+		System.out.println("Adicionando Nova Pagina");
+	}
+	
+	public void atualizarPagina(){
+		System.out.println("Atualizando Pagina");	
+	}
+
+}
